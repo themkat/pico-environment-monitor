d40e5d08541b4738441ca6767bb4495856b334d7
Marie Katrine Ekeberg <mke@themkat.net>
2023-04-21 14:42:32 +0200

    Created super simple deployment scripts based upon my manual workflow

---
+++ deploy_main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CIRCUITPY_LOCATION=$1
+
+cp code/envmon.py $CIRCUITPY_LOCATION/.
+cp code/envmon_tests.py $CIRCUITPY_LOCATION/.
+cp code/report.py $CIRCUITPY_LOCATION/.
+cp code/report_tests.py $CIRCUITPY_LOCATION/.
+cp code/code.py $CIRCUITPY_LOCATION/.
