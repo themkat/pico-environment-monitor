cd3bea10ec63c114c4b41fb3e7f6bfbb2c3311b8
Marie Katrine Ekeberg <mke@themkat.net>
2023-04-22 13:19:16 +0200

    Remove tests from main-deploy. I blame copy pasting

--- deploy_main.sh
+++ deploy_main.sh
@@ -3,7 +3,5 @@
 CIRCUITPY_LOCATION=$1
 
 cp code/envmon.py $CIRCUITPY_LOCATION/.
-cp code/envmon_tests.py $CIRCUITPY_LOCATION/.
 cp code/report.py $CIRCUITPY_LOCATION/.
-cp code/report_tests.py $CIRCUITPY_LOCATION/.
 cp code/code.py $CIRCUITPY_LOCATION/.
